adfe4df88daa73a1297333f63abf9da7097cc70b
Eli Bosch <152683952+eli-bosch@users.noreply.github.com>
2025-02-04 21:12:37 -0600

    APIs are added
    Just need to implement email functionality and a function to make sure the information is updated upon request

--- internal/api/cordinates/cordinates.go
+++ internal/api/cordinates/cordinates.go
@@ -1,1 +1,43 @@
 package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+type CordinateResponse struct {
+	//Figure this out from openweather api
+	Name      string  `json:"name"`
+	Latitude  float64 `json:"lat"`
+	Longitude float64 `json:"lon"`
+	Country   string  `json:"country"`
+	State     string  `json:"state"`
+}
+
+func FetchState(City string, State string, Country string) (*CordinateResponse, error) {
+	apiKey := os.Getenv("WEATHER_API_KEY")
+	url := fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?q=%s,%s,%s&appid=%s", City, State, Country, apiKey)
+	fmt.Printf(url)
+
+	res, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var cordiateData CordinateResponse
+	err = json.Unmarshal(body, &cordiateData)
+	if err != nil {
+		return nil, err
+	}
+
+	return &cordiateData, nil
+}
--- internal/api/weather/weather.go
+++ internal/api/weather/weather.go
@@ -1,1 +1,47 @@
 package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+type WeatherResponse struct {
+	//Figure this out from openweather api
+	Weather []struct {
+		Main        string `json:"main"`
+		Description string `json:"description"`
+	} `json:"weather"`
+
+	Main struct {
+		Temp      float64 `json:"temp"`
+		FeelsLike float64 `json:"feels_like"`
+	} `json:"main"`
+}
+
+func FetchWeather(Longitude float64, Latitude float64) (*WeatherResponse, error) {
+	apiKey := os.Getenv("WEATHER_API_KEY") //Make name more descriptive
+	url := fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?lat=%.2f&lon=%.2f&appid=%s", Latitude, Longitude, apiKey)
+	fmt.Printf(url)
+
+	res, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var weatherData WeatherResponse
+	err = json.Unmarshal(body, &weatherData)
+	if err != nil {
+		return nil, err
+	}
+
+	return &weatherData, nil
+}
