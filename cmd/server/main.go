490b427d1067fde0311f4f6e00c99c316350e4c3
Eli Bosch <152683952+eli-bosch@users.noreply.github.com>
2025-04-07 19:33:08 -0500

    Created sending function

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	_ "github.com/eli-bosch/go-weatherReminder/internal/db"
 	"github.com/eli-bosch/go-weatherReminder/internal/routes"
@@ -11,8 +13,24 @@ import (
 )
 
 func main() {
+	go startWeatherReminderJob()
+
 	r := mux.NewRouter()
 	routes.RegisterWeatherReminderRoutes(r)
+
 	http.Handle("/", r)
 	log.Fatal(http.ListenAndServe("localhost:9010", r))
+}
+
+func startWeatherReminderJob() {
+	ticker := time.NewTicker(15 * time.Minute)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			fmt.Println("Running reminder job at", time.Now())
+			//ADD text api
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -8,4 +8,6 @@ require (
 	github.com/jinzhu/gorm v1.9.16 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/sendgrid/rest v2.6.9+incompatible // indirect
+	github.com/sendgrid/sendgrid-go v3.16.0+incompatible // indirect
 )
